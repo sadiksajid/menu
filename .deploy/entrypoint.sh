5f0e4b8f8956e9398a0ffb16f32ec8026d99edac
sadiksajid <sadikagadir@gmail.com>
2024-04-03 00:38:16 +0000

    save

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -5,7 +5,8 @@ echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
 composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
-
+chmod 777 -R $LARAVEL_PATH/bootstrap/cache
+chmod 777 -R $LARAVEL_PATH/storage
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
 php artisan migrate --no-interaction --force
 # php artisan websocket:serve
