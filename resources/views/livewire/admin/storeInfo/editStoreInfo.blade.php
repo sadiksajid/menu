524a877e1faa9ab301d5f4ae8b048a5612aeec07
sadik <sajid.s@codpartner.com>
2024-06-29 09:18:20 +0000

    save

--- resources/views/livewire/admin/storeInfo/editStoreInfo.blade.php
+++ resources/views/livewire/admin/storeInfo/editStoreInfo.blade.php
@@ -493,6 +493,8 @@
 
 <script src="{{ URL::asset('assets/plugins/fileuploads/js/fileupload.js') }}?v=3"></script>
 <script src="{{ URL::asset('assets/plugins/fileuploads/js/file-upload.js') }}?v=6"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 <script>
 window.addEventListener('StoreInfoModal', event => {
     var status = event.detail.status;
