690bffa2a9fdfa0c05afd8bf9e82b60d44b58b7c
sadik <sajid.s@codpartner.com>
2024-07-06 11:11:33 +0000

    save

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,8 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => false,
                 'useTLS' => false,
-                'encrypted' => true,
                 'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_APP_PORT', 6001),
                 'scheme' => env('PUSHER_APP_SCHEME', 'http'),
